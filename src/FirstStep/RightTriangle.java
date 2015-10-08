c971e10c40096bb51a86c89a64a687daf1867526
olgasavchuk <olga.savchuk.pm@gmail.com>
2015-10-08 22:31:00 +0300

    RightTriangle class is added

---
+++ src/FirstStep/RightTriangle.java
@@ -0,0 +1,15 @@
+package FirstStep;
+
+public class RightTriangle {
+
+    public static void main(String[] args) throws Exception {
+        for (int i = 1; i < 11; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
